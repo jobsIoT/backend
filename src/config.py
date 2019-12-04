0d82bed3b2c528dd0e502a9b6591ca96ffeb28cd
Alexandre Buhl <alexandre.buhl@epita.fr>
2019-12-04 17:42:47 +0100

    create table

--- src/config.py
+++ src/config.py
@@ -7,6 +7,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://tfukrhhhusivup:2a5de14b538622c46b9f616ec77d4977b893ba4b5e2417864aa03a0dcdf17043@ec2-54-246-84-100.eu-west-1.compute.amazonaws.com:5432/d9kv8uh670d48o'
 
+
 class ProductionConfig(Config):
     DEBUG = False
 
