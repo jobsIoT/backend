9d861bd752a7359c09b024789c00775c332226bb
Alexandre Buhl <alexandre.buhl@epita.fr>
2019-12-04 17:35:59 +0100

    add database

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     DEBUG = False
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://postgres:290442@localhost:5432/test_iot'
+    SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://tfukrhhhusivup:2a5de14b538622c46b9f616ec77d4977b893ba4b5e2417864aa03a0dcdf17043@ec2-54-246-84-100.eu-west-1.compute.amazonaws.com:5432/d9kv8uh670d48o'
 
 class ProductionConfig(Config):
     DEBUG = False
