a2c970ff06369563b875990046b33cd4319baa67
Alexandre Buhl <alexandre.buhl@epita.fr>
2019-12-04 16:33:01 +0100

    modify name in connexion

--- src/ressources/connexion.py
+++ src/ressources/connexion.py
@@ -4,14 +4,14 @@ from flask import Response
 from src.models import User
 
 parser = reqparse.RequestParser()
-parser.add_argument('mail', type=str, required=True, help='mail of the user')
+parser.add_argument('email', type=str, required=True, help='mail of the user')
 parser.add_argument('password', type=str, required=True, help='password of ther user')
 
 
 class Connexion(Resource):
     def post(self):
         args = parser.parse_args(strict=True)
-        id_ = User.query.filter_by(email=args['mail']).first()
+        id_ = User.query.filter_by(email=args['email']).first()
         if id_ is None:
             return Response(
                 response=json.dumps(dict(error='User doesn\'t exist')),
--- src/ressources/inscription.py
+++ src/ressources/inscription.py
@@ -5,16 +5,16 @@ from src.db import db
 from src.models import User
 
 parser = reqparse.RequestParser()
-parser.add_argument('name', type=str, required=True, help='Name of the user')
+parser.add_argument('email', type=str, required=True, help='mail of the user')
+parser.add_argument('firstname', type=str, required=True, help='Name of the user')
 parser.add_argument('lastname', type=str, required=True, help='surname of the user')
-parser.add_argument('mail', type=str, required=True, help='mail of the user')
 parser.add_argument('password', type=str, required=True, help='password of ther user')
 
 
 class Inscription(Resource):
     def post(self):
         args = parser.parse_args(strict=True)
-        new_user = User(name=args['name'], lastname=args['lastname'], email=args['mail'],
+        new_user = User(name=args['firstname'], lastname=args['lastname'], email=args['email'],
                         password=args['password'])
         id_ = User.query.filter_by(email=args['mail']).first()
         if id_ is not None:
@@ -23,7 +23,7 @@ class Inscription(Resource):
                 status=400, mimetype='application/json')
         db.session.add(new_user)
         db.session.commit()
-        id_ = User.query.filter_by(email=args['mail']).first()
+        id_ = User.query.filter_by(email=args['email']).first()
         return Response(
                 response=id_.to_json(),
                 status=200, mimetype='application/json')
