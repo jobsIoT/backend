0f8744bbddd1299ffd59e1b1c0ece0b0371ecef9
Alexandre Buhl <alexandre.buhl@epita.fr>
2019-12-05 23:25:41 +0100

    rework models

--- src/models.py
+++ src/models.py
@@ -11,7 +11,7 @@ class User(OutputMixin, db.Model):
     firstname = db.Column(db.String, nullable=False)
     lastname = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
-    ispremium = db.Column(db.boolean, nullable=False)
+    ispremium = db.Column(db.Boolean, nullable=False)
 
     def __repr__(self):
         return "<Book( email='{}', firstname='{}', lastname='{}', password='{}')>" \
