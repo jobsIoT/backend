f061636d45755b4990f238443de6ce80d75ff3ff
Alexandre Buhl <alexandre.buhl@epita.fr>
2019-12-05 23:21:24 +0100

    add field ispremium in sign up

--- src/ressources/inscription.py
+++ src/ressources/inscription.py
@@ -9,13 +9,14 @@ parser.add_argument('email', type=str, required=True, help='mail of the user')
 parser.add_argument('firstname', type=str, required=True, help='Error : Name of the user')
 parser.add_argument('lastname', type=str, required=True, help='surname of the user')
 parser.add_argument('password', type=str, required=True, help='password of ther user')
+parser.add_argument('isPremium', type=bool, required=True, help='is a premium user')
 
 
 class Inscription(Resource):
     def post(self):
         args = parser.parse_args(strict=True)
         new_user = User(firstname=args['firstname'], lastname=args['lastname'], email=args['email'],
-                        password=args['password'])
+                        password=args['password'], isPremium=args['isPremium'])
         id_ = User.query.filter_by(email=args['email']).first()
         if id_ is not None:
             return Response(
