6034dd326b51ebd5e01199f5f5c8cb42c5616708
Alexandre Buhl <alexandre.buhl@epita.fr>
2019-12-06 12:02:07 +0100

    add get pulls

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ from src.ressources.connexion import Connexion
 from src.ressources.user_manage import User_manage
 from src.ressources.modify_user import Modify_user
 from src.ressources.send_pulls import Send_pulls
+from src.ressources.get_pulls import getPulls
 from src.ressources.home import Home
 
 api.add_resource(Inscription, '/inscription')
@@ -12,6 +13,7 @@ api.add_resource(Connexion, '/connexion')
 api.add_resource(User_manage, '/user')
 api.add_resource(Modify_user, '/modify_user')
 api.add_resource(Send_pulls, '/send_pulls')
+api.add_resource(getPulls, '/get_pulls')
 api.add_resource(Home, '/')
 
 if __name__ == '__main__':
---
+++ src/ressources/get_pulls.py
@@ -0,0 +1,21 @@
+from flask_restful import reqparse, Resource
+from flask import Response
+import json
+from src.models import Cardiaque
+
+parser = reqparse.RequestParser()
+parser.add_argument('email', type=str, required=True, help='mail of the user you want the information')
+
+
+class getPulls(Resource):
+    def post(self):
+        args = parser.parse_args(strict=True)
+        pulls = Cardiaque.query.filter_by(email=args['email'])
+
+        if pulls is None:
+            return Response(
+                response=json.dumps(dict(error='User doesn\'t exist')),
+                status=400, mimetype='application/json')
+        return Response(
+            response=pulls.to_json(),
+            status=200, mimetype='application/json')
