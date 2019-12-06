dbc156eca31a6ba724c37880256b470e08bd3576
Alexandre Buhl <alexandre.buhl@epita.fr>
2019-12-06 12:40:10 +0100

    json dumps getPulls

--- src/ressources/get_pulls.py
+++ src/ressources/get_pulls.py
@@ -17,5 +17,5 @@ class getPulls(Resource):
                 response=json.dumps(dict(error='User doesn\'t exist')),
                 status=400, mimetype='application/json')
         return Response(
-            response=pulls.to_json(),
+            response=json.dumps(pulls),
             status=200, mimetype='application/json')
