5219a6349c6549eafad47ad9d9f790d0b9860979
Alexandre Buhl <alexandre.buhl@epita.fr>
2019-12-14 16:37:38 +0100

    add table journeys

--- src/app.py
+++ src/app.py
@@ -6,6 +6,7 @@ from src.ressources.user_manage import User_manage
 from src.ressources.modify_user import Modify_user
 from src.ressources.send_pulls import Send_pulls
 from src.ressources.get_pulls import getPulls
+from src.ressources.get_journeys import getJourneys
 from src.ressources.home import Home
 
 api.add_resource(Inscription, '/inscription')
@@ -14,6 +15,7 @@ api.add_resource(User_manage, '/user')
 api.add_resource(Modify_user, '/modify_user')
 api.add_resource(Send_pulls, '/send_pulls')
 api.add_resource(getPulls, '/get_pulls')
+api.add_resource(getJourneys, '/get_journeys')
 api.add_resource(Home, '/')
 
 if __name__ == '__main__':
