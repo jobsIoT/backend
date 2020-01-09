e86d94b0dc3ac3afbfa0b6a6adc41bc7d0898bbc
Alexandre Buhl <alexandre.buhl@epita.fr>
2020-01-09 18:53:55 +0100

    add journey page to project

--- src/ressources/get_users.py
+++ src/ressources/get_users.py
@@ -12,5 +12,5 @@ class getUsers(Resource):
                 response=json.dumps(dict(error='User doesn\'t exist')),
                 status=400, mimetype='application/json')
         return Response(
-                response=id_,
+                response=bytearray(id_),
                 status=200, mimetype='application/json')
