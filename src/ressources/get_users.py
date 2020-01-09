617147b0293f0fd84531b3f393e017e644228b40
Alexandre Buhl <alexandre.buhl@epita.fr>
2020-01-09 19:05:08 +0100

    add journey page to project

--- src/ressources/get_users.py
+++ src/ressources/get_users.py
@@ -7,11 +7,11 @@ from src.models import User
 class getUsers(Resource):
     def post(self):
         id_ = User.query.all()
-        print(id_)
+        tmp = [{'email': elt.email, 'firstname': elt.firstname, 'lastname': elt.lastname, 'password': elt.password, 'ispremium': elt.ispremium, 'serialnumber': elt.serialnumber, 'isadmin': elt.isadmin} for elt in id_]
         if id_ is None:
             return Response(
                 response=json.dumps(dict(error='User doesn\'t exist')),
                 status=400, mimetype='application/json')
         return Response(
-                response=bytearray(id_),
+                response=json.dumps(tmp),
                 status=200, mimetype='application/json')
