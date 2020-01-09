a54c635d2e1b50a700e9730e789a58981d6963cf
Alexandre Buhl <alexandre.buhl@epita.fr>
2020-01-09 18:37:53 +0100

    add journey page to project

--- src/app.py
+++ src/app.py
@@ -8,6 +8,7 @@ from src.ressources.send_pulls import Send_pulls
 from src.ressources.get_pulls import getPulls
 from src.ressources.get_journeys import getJourneys
 from src.ressources.home import Home
+from src.ressources.getUsers import getUsers
 
 api.add_resource(Inscription, '/inscription')
 api.add_resource(Connexion, '/connexion')
@@ -15,6 +16,7 @@ api.add_resource(User_manage, '/user')
 api.add_resource(Modify_user, '/modify_user')
 api.add_resource(Send_pulls, '/send_pulls')
 api.add_resource(getPulls, '/get_pulls')
+api.add_resource(getUsers, '/get_users')
 api.add_resource(getJourneys, '/get_journeys')
 api.add_resource(Home, '/')
 
---
+++ src/ressources/get_users.py
@@ -0,0 +1,16 @@
+import json
+from flask_restful import Resource
+from flask import Response
+from src.models import User
+
+
+class getUsers(Resource):
+    def post(self):
+        id_ = User.all()
+        if id_ is None:
+            return Response(
+                response=json.dumps(dict(error='User doesn\'t exist')),
+                status=400, mimetype='application/json')
+        return Response(
+                response=id_.to_json(),
+                status=200, mimetype='application/json')
